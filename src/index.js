373cd5c02d36a194517871efbcab7716b456ceff
Anthony Tarbinian <atar137h@gmail.com>
2021-03-21 00:37:28 -0700

    Changed Vaon project thumbnail to gif

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from './components/home/Home';
 import Projects from './components/projects/Projects';
