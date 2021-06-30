6f9b600103cdb53b6146ba01f5e19d11a3e26f29
Anthony Tarbinian <atar137h@gmail.com>
2021-06-30 00:38:59 -0700

    Initial Vaon release depiction

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -5,44 +5,40 @@ import "aos/dist/aos.css"
 
 function Project(props) {
 
-    const {title, language, description, id, source, alt, thumbnail} = props
+    const { title, language, description, id, source, alt, thumbnail } = props
 
     useEffect(() => {
-        Aos.init({ duration: 0})
+        Aos.init({ duration: 0 })
     }, []);
 
     let fadeDirection = "fade-right"
-    if(id % 2 === 0){
+    if (id % 2 === 0) {
         fadeDirection = "fade-left"
     }
 
     let detailStatus = ""
-    if(alt === undefined || alt === ""){
+    if (alt === undefined || alt === "") {
         detailStatus = "none"
     }
 
     let thumbnailStatus = ""
-    if(thumbnail === undefined || thumbnail === "") {
+    if (thumbnail === undefined || thumbnail === "") {
         thumbnailStatus = "none"
     }
     return (
         <div data-aos={fadeDirection} data-aos-delay="150" data-aos-duration="1000" data-aos-easing="ease-out" class="project-container">
             <h2 class="project-title">{title}</h2>
             <h4 class="project-language">{language}</h4>
-            <img alt={title} className="project-thumbnail" style={{display:thumbnailStatus}} src={thumbnail} />
+            <img alt={title} className="project-thumbnail" style={{ display: thumbnailStatus }} src={thumbnail} />
             <p class="project-description">{description}</p>
-                <a href={source}>
-                <div className="link-wrapper">
-                    <label style={{paddingRight:"1rem"}}>Source Code</label>
-                    {/* <img className="github-icon" src="/assets/icons/github-240.png"></img> */}
-                    </div>
-                </a>
-                <a style={{display:detailStatus}} href={alt}>
-                <div className="link-wrapper">
-                    <label>Details</label>
-                    </div>
-                </a>        
-            </div>
+            <a className="link-wrapper" href={source}>
+                <img className="github-icon" src="/assets/icons/github-240.png"></img>
+                <label>Source Code</label>
+            </a>
+            <a className="link-wrapper" style={{ display: detailStatus }} href={alt}>
+                <label>Details</label>
+            </a>
+        </div>
     )
 }
 
--- src/components/repo/Repo.js
+++ src/components/repo/Repo.js
@@ -8,11 +8,11 @@ import tweaks from './tweaks.json'
 //add to cydia, zebra
 //grid of tweaks 
 
-function AddToPkg(props){
+function AddToPkg(props) {
 
     const { name, link } = props
     return (
-        <div style={{display:"flex", justifyContent:"center", paddingBottom:"3%"}}>
+        <div style={{ display: "flex", justifyContent: "center", paddingBottom: "3%" }}>
             <a class="add-to-pkg-button-wrapper" href={link}>
                 <button class="add-to-pkg-button">Add To {name}</button>
             </a>
@@ -25,20 +25,20 @@ function AddToPkg(props){
 function Repo() {
 
     const repoURL = "https://atar13.github.io/repo"
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
 
     return (
         <div>
-            <Navbar/>
-            <h1 style={{display:"flex", justifyContent:"center", paddingBottom:"2%"}}>atar13's tweak repository</h1>
-            <AddToPkg name="Cyida" link={`cydia://url/https://cydia.saurik.com/api/share#?source=${repoURL}`}/>
-            <AddToPkg name="Sileo" link={`sileo://source/${repoURL}`}/>
+            <Navbar />
+            <h1 style={{ display: "flex", justifyContent: "center", paddingBottom: "2%" }}>atar13's tweak repository</h1>
+            <AddToPkg name="Cyida" link={`cydia://url/https://cydia.saurik.com/api/share#?source=${repoURL}`} />
+            <AddToPkg name="Sileo" link={`sileo://source/${repoURL}`} />
             <AddToPkg name="Zebra" link={`zbra://sources/add/${repoURL}`} />
-            <AddToPkg name="Installer" link={`installer://${repoURL}`}/>
-            <div style={{display:"grid", placeContent:"center"}}>
-                <h1 style={{display:"grid", placeContent:"center", paddingBottom:"5%"}}>Packages:</h1>
+            <AddToPkg name="Installer" link={`installer://${repoURL}`} />
+            <div style={{ display: "grid", placeContent: "center" }}>
+                <h1 style={{ display: "grid", placeContent: "center", paddingBottom: "5%" }}>Packages:</h1>
                 {tweaks.map((tweak) => (
-                    <Package name={tweak.name} hidden={tweak.hidden} bundle={tweak.bundle} description={tweak.description} />
+                    <Package tweak={tweak} />
                 ))}
             </div>
             <Footer />
