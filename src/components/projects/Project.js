da92ea80a6635a5044ab09efacaab55b6e96ea5c
Anthony Tarbinian <atar137h@gmail.com>
2021-12-30 13:21:23 -0800

    go-dwm-statusbar thumbnail

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -28,9 +28,9 @@ function Project(props) {
     return (
         <div data-aos={fadeDirection} data-aos-delay="150" data-aos-duration="1000" data-aos-easing="ease-out" class="project-container">
             <h2 class="project-title">{title}</h2>
-            <h4 class="project-language">{language}</h4>
             <img alt={title} className="project-thumbnail" style={{ display: thumbnailStatus }} src={thumbnail} />
             <p class="project-description">{description}</p>
+            {language && <h4 class="project-language">Written in {language}</h4>}
             <a className="link-wrapper" href={source}>
                 <img className="github-icon" src="/assets/icons/github-240.png"></img>
                 <label>Source Code</label>
