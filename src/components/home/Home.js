8c3e47e1a1ddbce4e60cdcef0b4c12f10b09d55b
Anthony Tarbinian <atar137h@gmail.com>
2025-01-26 11:31:22 -0800

    masters

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,7 @@ function Home() {
                 <label class="name">Anthony Tarbinian</label>
                 <div class="description">
                     <h3 className="welcome">Welcome!</h3>
-                    <p>I'm a fourth year undergraduate student studying Computer Science at UC San Diego.</p>
+                    <p>I'm a masters student studying Computer Science at UC San Diego.</p>
                     <p>On this website you can view my personal projects 
                         and access my repository of iOS jailbreak tweaks.</p>
                 </div>
